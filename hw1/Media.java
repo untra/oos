97b26eaf98603d13abd820eb6aac71d90ebfb1d3
Cris Salazar <crsa7687@colorado.edu>
2015-01-25 14:41:51 -0700

    Update Media.java

--- hw1/Media.java
+++ hw1/Media.java
@@ -4,6 +4,17 @@ abstract class Media implements Comparable<Media>
 	public String getTitle()  { return title; }
 	
 	public int compareTo(Media other) {
-	   int i = toString().compareTo(other.toString());
+		if((this instanceof DVD && other instanceof DVD) || this instanceof Book && other instanceof Book){
+			if(this.getTitle().equals(other.getTitle()))
+				return this.toString().compareTo(other.toString());
+			else{
+				return this.getTitle().compareTo(other.getTitle());
+			}
+		}
+		else if(this instanceof DVD && other instanceof Book){
+			return -1;
+		}
+		else
+			return 1;
 	}
 }
